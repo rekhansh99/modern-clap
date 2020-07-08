8e96c89ce22eb8800577737aa015318a54d5d36a
Anirudh <anirudhganwal06@gmail.com>
2020-07-08 23:27:36 +0530

    started creating bookingdetails page

---
+++ frontend/src/pages/Bookings/BookingDetails.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import RepeatSlider from '../../components/RepeatSlider/RepeatSlider';
+
+const BookingDetails = () => {
+  const sliders = [
+    {
+      heading: 'cleaners',
+      desc: 'professional staff',
+      items: [
+        {
+          title: 'kitchen cleaning',
+          src: '/images/slider/1.jpg',
+          link: '#'
+        },
+        {
+          title: 'kitchen cleaning',
+          src: '/images/slider/1.jpg',
+          link: '#'
+        },
+        {
+          title: 'kitchen cleaning',
+          src: '/images/slider/1.jpg',
+          link: '#'
+        },
+        {
+          title: 'kitchen cleaning',
+          src: '/images/slider/1.jpg',
+          link: '#'
+        },
+        {
+          title: 'kitchen cleaning',
+          src: '/images/slider/1.jpg',
+          link: '#'
+        }
+      ]
+    }
+  ];
+  return <RepeatSlider {...sliders[0]} />;
+};
+
+export default BookingDetails;
