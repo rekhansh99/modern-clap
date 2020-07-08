660aed4b2ca51b27eb92dfae02bb9ab19923b17f
rekhansh99 <priyadarshirekhansh@gmail.com>
2020-07-09 02:04:07 +0530

    Added Header, Footer

--- frontend/src/pages/Blogs/index.js
+++ frontend/src/pages/Blogs/index.js
@@ -1,82 +1,92 @@
 import React from 'react';
+
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
+import Header from '../../app/components/Header';
+import Footer from '../../app/components/Footer';
+
 const Blogs = () => {
   return (
-    <div className="dv_content">
-      <div className="dv_terms_conditions">
-        <Container>
-          <Row>
-            <Col xs={12} sm={6} md={4}>
-              <div className="dv_blog_cards">
-                <a href="#!">
-                  <img src="images/blog-list.jpg" alt="" title />
-                  <div className="dv_blog_heading">blog heading text</div>
-                  <p className="dv_blog_p">
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit,
-                    sed do eiusmod..!
-                  </p>
-                  <span className="dv_blog_date">12/12/2020</span>
-                </a>
-              </div>
-            </Col>
-            <Col xs={12} sm={6} md={4}>
-              <div className="dv_blog_cards">
-                <a href="#!">
-                  <img src="images/blog-list.jpg" alt="" title />
-                  <div className="dv_blog_heading">blog heading text</div>
-                  <p className="dv_blog_p">
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit,
-                    sed do eiusmod..!
-                  </p>
-                  <span className="dv_blog_date">12/12/2020</span>
-                </a>
-              </div>
-            </Col>
-            <Col xs={12} sm={6} md={4}>
-              <div className="dv_blog_cards">
-                <a href="#!">
-                  <img src="images/blog-list.jpg" alt="" title />
-                  <div className="dv_blog_heading">blog heading text</div>
-                  <p className="dv_blog_p">
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit,
-                    sed do eiusmod..!
-                  </p>
-                  <span className="dv_blog_date">12/12/2020</span>
-                </a>
-              </div>
-            </Col>
-            <Col xs={12} sm={6} md={4}>
-              <div className="dv_blog_cards">
-                <a href="#!">
-                  <img src="images/blog-list.jpg" alt="" title />
-                  <div className="dv_blog_heading">blog heading text</div>
-                  <p className="dv_blog_p">
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit,
-                    sed do eiusmod..!
-                  </p>
-                  <span className="dv_blog_date">12/12/2020</span>
-                </a>
-              </div>
-            </Col>
-            <Col xs={12} sm={6} md={4}>
-              <div className="dv_blog_cards">
-                <a href="#!">
-                  <img src="images/blog-list.jpg" alt="" title />
-                  <div className="dv_blog_heading">blog heading text</div>
-                  <p className="dv_blog_p">
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit,
-                    sed do eiusmod..!
-                  </p>
-                  <span className="dv_blog_date">12/12/2020</span>
-                </a>
-              </div>
-            </Col>
-          </Row>
-        </Container>
+    <div className="dv_wrapper">
+      <Header />
+
+      <div className="dv_content">
+        <div className="dv_terms_conditions">
+          <Container>
+            <Row>
+              <Col xs={12} sm={6} md={4}>
+                <div className="dv_blog_cards">
+                  <a href="#!">
+                    <img src="images/blog-list.jpg" alt="" title />
+                    <div className="dv_blog_heading">blog heading text</div>
+                    <p className="dv_blog_p">
+                      Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+                      sed do eiusmod..!
+                    </p>
+                    <span className="dv_blog_date">12/12/2020</span>
+                  </a>
+                </div>
+              </Col>
+              <Col xs={12} sm={6} md={4}>
+                <div className="dv_blog_cards">
+                  <a href="#!">
+                    <img src="images/blog-list.jpg" alt="" title />
+                    <div className="dv_blog_heading">blog heading text</div>
+                    <p className="dv_blog_p">
+                      Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+                      sed do eiusmod..!
+                    </p>
+                    <span className="dv_blog_date">12/12/2020</span>
+                  </a>
+                </div>
+              </Col>
+              <Col xs={12} sm={6} md={4}>
+                <div className="dv_blog_cards">
+                  <a href="#!">
+                    <img src="images/blog-list.jpg" alt="" title />
+                    <div className="dv_blog_heading">blog heading text</div>
+                    <p className="dv_blog_p">
+                      Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+                      sed do eiusmod..!
+                    </p>
+                    <span className="dv_blog_date">12/12/2020</span>
+                  </a>
+                </div>
+              </Col>
+              <Col xs={12} sm={6} md={4}>
+                <div className="dv_blog_cards">
+                  <a href="#!">
+                    <img src="images/blog-list.jpg" alt="" title />
+                    <div className="dv_blog_heading">blog heading text</div>
+                    <p className="dv_blog_p">
+                      Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+                      sed do eiusmod..!
+                    </p>
+                    <span className="dv_blog_date">12/12/2020</span>
+                  </a>
+                </div>
+              </Col>
+              <Col xs={12} sm={6} md={4}>
+                <div className="dv_blog_cards">
+                  <a href="#!">
+                    <img src="images/blog-list.jpg" alt="" title />
+                    <div className="dv_blog_heading">blog heading text</div>
+                    <p className="dv_blog_p">
+                      Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+                      sed do eiusmod..!
+                    </p>
+                    <span className="dv_blog_date">12/12/2020</span>
+                  </a>
+                </div>
+              </Col>
+            </Row>
+          </Container>
+        </div>
       </div>
+
+      <Footer />
     </div>
   );
 };
