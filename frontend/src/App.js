9a56e52b3f4f69a6d8039f53388b8295f8047b89
Anirudh <anirudhganwal06@gmail.com>
2020-07-08 23:49:12 +0530

    created privacy policy page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,12 +3,14 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import './App.css';
 import AboutUs from './pages/AboutUs';
+import PrivacyPolicy from './pages/PrivacyPolicy';
 
 function App() {
   return (
     <Router>
       <Switch>
         <Route exact path="/about-us" component={AboutUs} />
+        <Route exact path="/privacy-policy" component={PrivacyPolicy} />
       </Switch>
     </Router>
   );
---
+++ frontend/src/pages/PrivacyPolicy/index.js
@@ -0,0 +1,81 @@
+import React from 'react';
+
+const PrivacyPolicy = () => {
+  return (
+    <div className="dv_content">
+      <div className="dv_terms_conditions">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12">
+              <div className="dv_terms_head">Privacy Policy heading</div>
+              <p className="dv_terms_p">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam,quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                sunt in culpa qui officia deserunt mollit anim id est laborum.{' '}
+              </p>
+            </div>
+            <div className="dv_divider" />
+            <div className="col-md-12">
+              <div className="dv_terms_head">Privacy Policy heading</div>
+              <p className="dv_terms_p">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam,quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                sunt in culpa qui officia deserunt mollit anim id est laborum.{' '}
+              </p>
+            </div>
+            <div className="dv_divider" />
+            <div className="col-md-12">
+              <div className="dv_terms_head">Privacy Policy heading</div>
+              <p className="dv_terms_p">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam,quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                sunt in culpa qui officia deserunt mollit anim id est laborum.{' '}
+              </p>
+            </div>
+            <div className="dv_divider" />
+            <div className="col-md-12">
+              <div className="dv_terms_head">Privacy Policy heading</div>
+              <p className="dv_terms_p">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam,quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                sunt in culpa qui officia deserunt mollit anim id est laborum.{' '}
+              </p>
+            </div>
+            <div className="dv_divider" />
+            <div className="col-md-12">
+              <div className="dv_terms_head">Privacy Policy heading</div>
+              <p className="dv_terms_p">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam,quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                sunt in culpa qui officia deserunt mollit anim id est laborum.{' '}
+              </p>
+            </div>
+            <div className="dv_divider" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PrivacyPolicy;
