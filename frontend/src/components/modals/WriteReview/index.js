ff09a1c17bdf4dcacc06e404e436406cc0b2a90a
rekhansh99 <priyadarshirekhansh@gmail.com>
2020-07-10 06:40:32 +0530

    bookings page and write-review modal created

---
+++ frontend/src/components/modals/WriteReview/index.js
@@ -0,0 +1,124 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import Modal from 'react-bootstrap/Modal';
+import ModalHeader from 'react-bootstrap/ModalHeader';
+import ModalTitle from 'react-bootstrap/ModalTitle';
+import ModalBody from 'react-bootstrap/ModalBody';
+import FormControl from 'react-bootstrap/FormControl';
+import Button from 'react-bootstrap/Button';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+const WriteReview = ({ isOpen, setOpen }) => {
+  return (
+    <Modal
+      id="dv_write_reviews"
+      dialogClassName="dv_choose_location_md_dialog"
+      show={isOpen}
+      onHide={() => setOpen(false)}
+    >
+      <ModalHeader>
+        <ModalTitle style={{ color: '#282828', fontSize: '18px' }}>
+          <img
+            src="/images/svgs/reviews.svg"
+            width={20}
+            height={20}
+            alt=""
+            style={{
+              float: 'left',
+              margin: '0 10px 0 0'
+            }}
+          />
+          Add Review
+        </ModalTitle>
+        <img
+          src="/images/svgs/close.svg"
+          width={24}
+          height={24}
+          alt=""
+          onClick={() => setOpen(false)}
+        />
+      </ModalHeader>
+      <ModalBody
+        style={{
+          border: 'none',
+          padding: '15px',
+          borderRadius: '0 0 5px 5px'
+        }}
+      >
+        <div className="dv_review_based_modal">
+          <FormControl
+            as="textarea"
+            className="dv_careers_form_input"
+            style={{ height: '100px' }}
+            placeholder="Add Review"
+            defaultValue={''}
+          />
+          <Row>
+            <Col
+              lg={6}
+              md={6}
+              sm={6}
+              xs={12}
+              className="dv_raing_per_field_wrapper"
+            >
+              <label>Tools </label>
+              <div className="dv_give_rating" />
+            </Col>
+            <Col
+              lg={6}
+              md={6}
+              sm={6}
+              xs={12}
+              className="dv_raing_per_field_wrapper"
+            >
+              <label>Work </label>
+              <div className="dv_give_rating" />
+            </Col>
+            <Col
+              lg={6}
+              md={6}
+              sm={6}
+              xs={12}
+              className="dv_raing_per_field_wrapper"
+            >
+              <label>Behaviour </label>
+              <div className="dv_give_rating" />
+            </Col>
+            <Col
+              lg={6}
+              md={6}
+              sm={6}
+              xs={12}
+              className="dv_raing_per_field_wrapper"
+            >
+              <label>Overall rating </label>
+              <div className="dv_give_rating" />
+            </Col>
+          </Row>
+          <Button
+            style={{
+              padding: '10px 0',
+              textTransform: 'capitalize',
+              margin: '25px 0 0 0'
+            }}
+            type="button"
+            variant="primary"
+            block
+          >
+            submit
+          </Button>
+          {/* <input type="text" readonly id="basicInput" className="form-control form-control-sm"> */}
+        </div>
+      </ModalBody>
+    </Modal>
+  );
+};
+
+WriteReview.propTypes = {
+  isOpen: PropTypes.bool.isRequired,
+  setOpen: PropTypes.func.isRequired
+};
+
+export default WriteReview;
--- frontend/src/pages/BookingDetails/index.js
+++ frontend/src/pages/BookingDetails/index.js
@@ -1,41 +1,292 @@
 import React from 'react';
-import RepeatSlider from '../../components/RepeatSlider';
 
-const BookingDetails = () => {
-  const sliders = [
-    {
-      heading: 'cleaners',
-      desc: 'professional staff',
-      items: [
-        {
-          title: 'kitchen cleaning',
-          src: '/images/slider/1.jpg',
-          link: '#'
-        },
-        {
-          title: 'kitchen cleaning',
-          src: '/images/slider/1.jpg',
-          link: '#'
-        },
-        {
-          title: 'kitchen cleaning',
-          src: '/images/slider/1.jpg',
-          link: '#'
-        },
-        {
-          title: 'kitchen cleaning',
-          src: '/images/slider/1.jpg',
-          link: '#'
-        },
-        {
-          title: 'kitchen cleaning',
-          src: '/images/slider/1.jpg',
-          link: '#'
-        }
-      ]
-    }
-  ];
-  return <RepeatSlider {...sliders[0]} />;
-};
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+
+import Header from '../../components/Header';
+import RepeatSlider from '../../components/RepeatSlider';
+import WriteReview from '../../components/modals/WriteReview';
+
+class BookingDetails extends React.Component {
+  state = {
+    writeReviewOpen: false
+  };
+
+  render = () => {
+    return (
+      <div className="dv_wrapper">
+        <Header
+          headerClassName="dv_booking_request_details"
+          title="Request Booked"
+          desc="Thank you for booking request with us. we will give our best professional service to you..!"
+        />
+
+        <div
+          className="dv_select_service_heading"
+          style={{ textAlign: 'center' }}
+        >
+          <Container>
+            <a href="#!" data-toggle="modal" data-target="#donesuccesfully">
+              Your order request no is <strong>MD124587</strong>
+            </a>
+          </Container>
+        </div>
+        {/* <div className="dv_select_service_heading" style="text-align: center; background-color: #ff4a4a;">
+        <Container>
+          <a href="javascript:void(0);">
+            Your cancelled order request no is <strong>MD124587</strong>
+          </a>
+        </Container>
+      </div> */}
+
+        <div className="dv_booking_details_page">
+          <Container>
+            {/* This review system will enable after completing of payment */}
+            <Row>
+              <span className="dv_section_heading">
+                <img src="/images/svgs/tick.svg" alt="" />
+                services <span className="dv_sm_overview">2 services </span>
+                <a
+                  href="#!"
+                  data-toggle="modal"
+                  data-target="#viewrequestdetails"
+                  className="dv_edit_for_all"
+                >
+                  View
+                </a>
+              </span>
+              <div className="dv_divider_in_booking_request" />
+            </Row>
+            <Row>
+              <span className="dv_section_heading">
+                <img src="/images/svgs/tick.svg" alt="" />
+                Timing <span className="dv_sm_overview">12 Mar, 04 pm </span>
+                <a
+                  href="#!"
+                  data-toggle="modal"
+                  data-target="#viewrequestdetails"
+                  className="dv_edit_for_all"
+                >
+                  Reschedule
+                </a>
+              </span>
+              <div className="dv_divider_in_booking_request" />
+            </Row>
+            <Row>
+              <span className="dv_section_heading">
+                <img src="/images/svgs/tick.svg" alt="" />
+                location <span className="dv_sm_overview">Home </span>
+                <a
+                  href="#!"
+                  data-toggle="modal"
+                  data-target="#viewrequestdetails"
+                  className="dv_edit_for_all"
+                >
+                  View
+                </a>
+              </span>
+              <div className="dv_divider_in_booking_request" />
+            </Row>
+            <Row>
+              <span className="dv_section_heading">
+                <img src="/images/svgs/tick.svg" alt="" />
+                Payment <span className="dv_sm_overview">AED 114 </span>
+                <a
+                  href="#!"
+                  data-toggle="modal"
+                  data-target="#viewrequestdetails"
+                  className="dv_edit_for_all"
+                >
+                  View
+                </a>
+              </span>
+              <div className="dv_divider_in_booking_request" />
+            </Row>
+            <Row>
+              <span className="dv_section_heading">
+                <img src="/images/svgs/tick.svg" alt="" />
+                Review
+                <span className="dv_sm_overview">
+                  Write review after payment completed
+                </span>
+                <a
+                  href="#!"
+                  onClick={() => this.setState({ writeReviewOpen: true })}
+                  className="dv_edit_for_all"
+                >
+                  Write
+                </a>
+              </span>
+              <div className="dv_divider_in_booking_request" />
+            </Row>
+            <Row>
+              <span className="dv_section_heading">
+                <img src="/images/svgs/tick.svg" alt="" />
+                FAQ&apos;s <span className="dv_sm_overview">14 Questions </span>
+                <a
+                  href="#!"
+                  data-toggle="modal"
+                  data-target="#viewrequestdetails"
+                  className="dv_edit_for_all"
+                >
+                  View
+                </a>
+              </span>
+              <div className="dv_divider_in_booking_request" />
+            </Row>
+            <Row>
+              <span className="dv_section_heading">
+                <img src="/images/svgs/tick.svg" alt="" />
+                Provider
+                {/* <span className="dv_sm_overview">Goodhand Technical LLC </span> */}
+                <a
+                  href="#!"
+                  data-toggle="modal"
+                  data-target="#viewrequestdetails"
+                  className="dv_edit_for_all"
+                >
+                  View
+                </a>
+              </span>
+              <div className="dv_divider_in_booking_request" />
+            </Row>
+          </Container>
+        </div>
+
+        <div className="dv_all_sliders">
+          <Container>
+            <div className="dv_get_more_services">explore more services</div>
+          </Container>
+          <RepeatSlider
+            heading="cleaners"
+            desc="professional staff"
+            items={[
+              {
+                link: '#',
+                src: 'images/slider/1.jpg',
+                title: 'deep cleaning'
+              },
+              {
+                link: '#',
+                src: 'images/slider/2.jpg',
+                title: 'soft cleaning'
+              },
+              {
+                link: '#',
+                src: 'images/slider/3.jpg',
+                title: 'kitchen cleaning'
+              },
+              {
+                link: '#',
+                src: 'images/slider/4.jpg',
+                title: 'bathroom cleaning'
+              }
+            ]}
+          />
+          <RepeatSlider
+            heading="outfeet fitness"
+            desc="safety first"
+            items={[
+              {
+                link: '#',
+                src: 'images/slider/1.jpg',
+                title: 'service title'
+              },
+              {
+                link: '#',
+                src: 'images/slider/2.jpg',
+                title: 'service title'
+              },
+              {
+                link: '#',
+                src: 'images/slider/3.jpg',
+                title: 'service title'
+              },
+              {
+                link: '#',
+                src: 'images/slider/4.jpg',
+                title: 'service title'
+              }
+            ]}
+          />
+          <RepeatSlider
+            heading="bathroom cleaning"
+            desc="used hand gloves"
+            items={[
+              {
+                link: '#',
+                src: 'images/slider/1.jpg',
+                title: 'service title'
+              },
+              {
+                link: '#',
+                src: 'images/slider/2.jpg',
+                title: 'service title'
+              },
+              {
+                link: '#',
+                src: 'images/slider/3.jpg',
+                title: 'service title'
+              },
+              {
+                link: '#',
+                src: 'images/slider/4.jpg',
+                title: 'service title'
+              }
+            ]}
+          />
+          <RepeatSlider
+            heading="repairs"
+            desc="30 days service guarantee"
+            items={[
+              {
+                link: '#',
+                src: 'images/slider/1.jpg',
+                title: 'service title'
+              },
+              {
+                link: '#',
+                src: 'images/slider/2.jpg',
+                title: 'service title'
+              },
+              {
+                link: '#',
+                src: 'images/slider/3.jpg',
+                title: 'service title'
+              },
+              {
+                link: '#',
+                src: 'images/slider/4.jpg',
+                title: 'service title'
+              }
+            ]}
+          />
+        </div>
+
+        <WriteReview
+          isOpen={this.state.writeReviewOpen}
+          setOpen={open => this.setState({ writeReviewOpen: open })}
+        />
+
+        <div className="dv_continue_next">
+          <Container>
+            <ul>
+              <li>
+                <a href="#!" data-toggle="modal" data-target="#areyousure">
+                  Cancel
+                </a>
+              </li>
+              <li>
+                <a href="#!" data-toggle="modal" data-target="#resheduletime">
+                  Reshedule
+                </a>
+              </li>
+            </ul>
+          </Container>
+        </div>
+      </div>
+    );
+  };
+}
 
 export default BookingDetails;
