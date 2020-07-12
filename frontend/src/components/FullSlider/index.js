449505ce93438510f2d569ffa1b3b594f8a4b646
rekhansh99 <priyadarshirekhansh@gmail.com>
2020-07-12 23:41:18 +0530

    switched to ItemsCarousel

--- frontend/src/components/FullSlider/index.js
+++ frontend/src/components/FullSlider/index.js
@@ -1,12 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import Container from 'react-bootstrap/Container';
-import OwlCarousel from 'react-owl-carousel-autoheight';
-
-import 'owl.carousel/dist/assets/owl.carousel.css';
+import ItemsCarousel from 'react-items-carousel';
+import { ChevronLeft, ChevronRight } from 'react-feather';
 
 const FullSlider = () => {
+  const [activeItem, setActiveItem] = useState(0);
+
   const images = [
     '/images/slider.jpg',
     '/images/slider.jpg',
@@ -25,24 +26,28 @@ const FullSlider = () => {
     <div className="dv_home_full_slider">
       <Container>
         <div className="dv_home_slider_card">
-          <OwlCarousel
-            items={1}
-            className="dv_home_slider_banner"
-            nav={true}
-            loop={true}
-            navClass={[
-              'owl-prev dv_home_slider_prev',
-              'owl-next dv_home_slider_next'
-            ]}
-            margin={0}
-            navText={[
-              "<img src='/images/chevron-left.svg'>",
-              "<img src='/images/chevron-right.svg'>"
-            ]}
-            dots={false}
+          <ItemsCarousel
+            activeItemIndex={activeItem}
+            requestToChangeActive={setActiveItem}
+            numberOfCards={1}
+            infiniteLoop={true}
+            leftChevron={
+              <button type="button" className="dv_sploffer_owl_prev">
+                <ChevronLeft />
+              </button>
+            }
+            rightChevron={
+              <button type="button" className="dv_sploffer_owl_next">
+                <ChevronRight />
+              </button>
+            }
+            classes={{
+              rightChevronWrapper: 'p-2',
+              leftChevronWrapper: 'p-2'
+            }}
           >
             {imagesJSX}
-          </OwlCarousel>
+          </ItemsCarousel>
         </div>
       </Container>
     </div>
--- frontend/src/components/RepeatSlider/index.js
+++ frontend/src/components/RepeatSlider/index.js
@@ -1,17 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
 import { Link } from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
-import OwlCarousel from 'react-owl-carousel-autoheight';
-
-import 'owl.carousel/dist/assets/owl.carousel.css';
+import ItemsCarousel from 'react-items-carousel';
+import { ChevronLeft, ChevronRight } from 'react-feather';
 
 const RepeatSlider = props => {
+  const [activeItem, setActiveItem] = useState(0);
+
   const sliderItemsJSX = props.items.map((item, i) => (
     <div className="item" key={i}>
       <Link to={item.link}>
-        <img className="owlsliderimg" src={item.src} alt={item.alt} />
+        <img
+          className="owlsliderimg"
+          height={199}
+          src={item.src}
+          alt={item.alt}
+        />
         <div className="dv_img_title">{item.title}</div>
       </Link>
     </div>
@@ -22,40 +28,33 @@ const RepeatSlider = props => {
       <Container>
         <h3 className="dv_repeat_slider_heading">{props.heading}</h3>
         <p className="dv_repeat_slider_para">{props.desc}</p>
-        <OwlCarousel
-          items={3}
-          className="owl-theme dv_home_slider"
-          // autoHeight={true}
-          // autoWidth={true}
-          nav={true}
-          loop={true}
-          navClass={[
-            'owl-prev dv_sploffer_owl_prev',
-            'owl-next dv_sploffer_owl_next'
-          ]}
-          margin={15}
-          navText={[
-            "<img src='/images/chevron-left.svg'>",
-            "<img src='/images/chevron-right.svg'>"
-          ]}
-          dots={false}
-          responsive={{
-            320: {
-              items: 1
-            },
-            480: {
-              items: 1
-            },
-            600: {
-              items: 2
-            },
-            991: {
-              items: 3
-            }
+        <ItemsCarousel
+          activeItemIndex={activeItem}
+          requestToChangeActive={setActiveItem}
+          numberOfCards={3}
+          infiniteLoop={true}
+          gutter={15}
+          showSlither={true}
+          slidesToScroll={1}
+          leftChevron={
+            <button type="button" className="dv_sploffer_owl_prev">
+              <ChevronLeft />
+            </button>
+          }
+          rightChevron={
+            <button type="button" className="dv_sploffer_owl_next">
+              <ChevronRight />
+            </button>
+          }
+          classes={{
+            itemsInnerWrapper: 'position-relative',
+            itemWrapper: 'w-auto position-relative',
+            rightChevronWrapper: 'p-2',
+            leftChevronWrapper: 'p-2'
           }}
         >
           {sliderItemsJSX}
-        </OwlCarousel>
+        </ItemsCarousel>
       </Container>
     </div>
   );
