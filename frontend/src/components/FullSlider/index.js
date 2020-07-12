56885c580588391283478da225a3ddcf126d24df
rekhansh99 <priyadarshirekhansh@gmail.com>
2020-07-13 03:03:59 +0530

    switched to swiper

--- frontend/src/components/FullSlider/index.js
+++ frontend/src/components/FullSlider/index.js
@@ -1,53 +1,62 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 
 import Container from 'react-bootstrap/Container';
-import ItemsCarousel from 'react-items-carousel';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import SwiperCore, { Navigation } from 'swiper';
 import { ChevronLeft, ChevronRight } from 'react-feather';
 
-const FullSlider = () => {
-  const [activeItem, setActiveItem] = useState(0);
+import 'swiper/swiper-bundle.min.css';
 
+SwiperCore.use([Navigation]);
+
+const FullSlider = () => {
   const images = [
     '/images/slider.jpg',
     '/images/slider.jpg',
     '/images/slider.jpg'
   ];
   const imagesJSX = images.map((image, i) => (
-    <div className="item dv_home_page_slider_item" key={i}>
-      <Link to="#!">
-        {/* for mobile version we will use diffrent img tag */}
-        <img src={image} alt="" />
-      </Link>
-    </div>
+    <SwiperSlide key={i}>
+      <div className="item dv_home_page_slider_item">
+        <Link to="#!">
+          {/* for mobile version we will use diffrent img tag */}
+          <img src={image} alt="" />
+        </Link>
+      </div>
+    </SwiperSlide>
   ));
 
   return (
     <div className="dv_home_full_slider">
       <Container>
         <div className="dv_home_slider_card">
-          <ItemsCarousel
-            activeItemIndex={activeItem}
-            requestToChangeActive={setActiveItem}
-            numberOfCards={1}
-            infiniteLoop={true}
-            leftChevron={
-              <button type="button" className="dv_sploffer_owl_prev">
-                <ChevronLeft />
-              </button>
-            }
-            rightChevron={
-              <button type="button" className="dv_sploffer_owl_next">
-                <ChevronRight />
-              </button>
-            }
-            classes={{
-              rightChevronWrapper: 'p-2',
-              leftChevronWrapper: 'p-2'
+          <Swiper
+            loop={true}
+            autoplay={true}
+            navigation={{
+              nextEl: '.dv_sploffer_owl_next',
+              prevEl: '.dv_sploffer_owl_prev'
             }}
           >
             {imagesJSX}
-          </ItemsCarousel>
+            <div className="owl-nav">
+              <button
+                slot="container-end"
+                type="button"
+                className="dv_sploffer_owl_prev"
+              >
+                <ChevronLeft />
+              </button>
+              <button
+                slot="container-end"
+                type="button"
+                className="dv_sploffer_owl_next"
+              >
+                <ChevronRight />
+              </button>
+            </div>
+          </Swiper>
         </div>
       </Container>
     </div>
