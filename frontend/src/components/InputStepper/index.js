8a9e56a41a8e6f8e3619824cc30869f35e6d4eb5
rekhansh99 <priyadarshirekhansh@gmail.com>
2020-07-15 12:59:34 +0530

    separate component for InputStepper created

---
+++ frontend/src/components/InputStepper/index.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import FormControl from 'react-bootstrap/FormControl';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
+import { Plus, Minus } from 'react-feather';
+
+const InputStepper = ({ value, onChange }) => {
+  return (
+    <ButtonGroup size="lg">
+      {value === 0 && <span className="dv_add_overlay">add</span>}
+      <button
+        type="button"
+        className="btn btn-default dv_plus_minusbtn"
+        onClick={() => onChange(value - 1)}
+      >
+        <Minus size={24} />
+      </button>
+      <FormControl type="text" className="dv_input_qty" value={value} />
+      <button
+        type="button"
+        className="btn btn-default dv_plus_minusbtn"
+        onClick={() => onChange(value + 1)}
+      >
+        <Plus size={24} />
+      </button>
+    </ButtonGroup>
+  );
+};
+
+InputStepper.propTypes = {
+  value: PropTypes.number.isRequired,
+  onChange: PropTypes.func.isRequired
+};
+
+export default InputStepper;
--- frontend/src/components/ServicesList/index.js
+++ frontend/src/components/ServicesList/index.js
@@ -2,202 +2,94 @@ import React from 'react';
 
 import { Link } from 'react-router-dom';
 import Col from 'react-bootstrap/Col';
-import Button from 'react-bootstrap/Button';
-import ButtonGroup from 'react-bootstrap/ButtonGroup';
-import FormControl from 'react-bootstrap/FormControl';
-import { Plus, Minus } from 'react-feather';
+import InputStepper from '../InputStepper';
+
+const services = [
+  {
+    name: 'African braids service 1',
+    actualPrice: 'AED 120',
+    discountedPrice: 'AED 90',
+    servicePoints: [
+      'OPI products',
+      'Disposable products used',
+      'safety is priority'
+    ],
+    link: '/service/medicure'
+  },
+  {
+    name: 'African braids service',
+    actualPrice: 'AED 120',
+    discountedPrice: 'AED 90',
+    link: '/service/medicure'
+  },
+  {
+    name: 'African braids service',
+    actualPrice: 'AED 120',
+    discountedPrice: 'AED 90',
+    link: '/service/medicure'
+  },
+  {
+    name: 'African braids service',
+    actualPrice: 'AED 120',
+    discountedPrice: 'AED 90',
+    link: '/service/medicure'
+  },
+  {
+    name: 'African braids service',
+    actualPrice: 'AED 120',
+    discountedPrice: 'AED 90',
+    link: '/service/medicure'
+  },
+  {
+    name: 'African braids service',
+    actualPrice: 'AED 120',
+    discountedPrice: 'AED 90',
+    link: '/service/medicure'
+  },
+  {
+    name: 'African braids service',
+    actualPrice: 'AED 120',
+    discountedPrice: 'AED 90',
+    link: '/service/medicure'
+  }
+];
 
 const ServicesList = () => {
+  const servicesJSX = services.map((service, index) => (
+    <li key={index}>
+      <div className="dv_service_prices_add_minus_btn_wrapper">
+        <div className="dv_service_prices_wrapper">
+          <span className="dv_service_name">{service.name}</span>
+        </div>
+        <div className="dv_plus_minus_wrapper">
+          <ul className="dv_prices_cut_main">
+            <li>{service.actualPrice}</li> <li>{service.discountedPrice}</li>
+          </ul>
+          <InputStepper value={index} onChange={() => {}} />
+        </div>
+        {service.servicePoints && (
+          <div className="dv_service_points">
+            <ul>
+              {service.servicePoints.map((point, index) => (
+                <li key={index}>{point}</li>
+              ))}
+              <li>
+                <Link to={service.link} className="read_more">
+                  read more
+                </Link>
+              </li>
+            </ul>
+          </div>
+        )}
+      </div>
+    </li>
+  ));
+
   return (
     <Col lg={6} sm={12} className="dv_select_service_3 p-0">
       <h3>services </h3>
       <ul className="dv_select_service_ul dv_last_service_border">
-        <li>
-          <div className="dv_service_prices_add_minus_btn_wrapper">
-            <div className="dv_service_prices_wrapper">
-              <span className="dv_service_name">African braids service 1</span>
-            </div>
-            <div className="dv_plus_minus_wrapper">
-              <ul className="dv_prices_cut_main">
-                <li>AED 120</li> <li>AED 90</li>
-              </ul>
-              <ButtonGroup size="lg">
-                <span className="dv_add_overlay">add </span>
-                <Button type="button" className="btn-default dv_plus_minusbtn">
-                  <Minus size={24} />
-                </Button>
-                <FormControl
-                  type="text"
-                  className="dv_input_qty"
-                  defaultValue={2}
-                />
-                <Button type="button" className="btn-default dv_plus_minusbtn">
-                  <Plus size={24} />
-                </Button>
-              </ButtonGroup>
-            </div>
-            <div className="dv_service_points">
-              <ul>
-                <li>OPI products </li>
-                <li>Disposable products used </li>
-                <li>safety is priority </li>
-                <li>
-                  <Link to={'/service/medicure'}>read more </Link>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </li>
-        <li>
-          <div className="dv_service_prices_add_minus_btn_wrapper">
-            <div className="dv_service_prices_wrapper">
-              <span className="dv_service_name">African braids service</span>
-            </div>
-            <div className="dv_plus_minus_wrapper">
-              <ul className="dv_prices_cut_main">
-                <li>AED 120</li> <li>AED 90</li>
-              </ul>
-              <ButtonGroup size="lg">
-                <Button type="button" className="btn-default dv_plus_minusbtn">
-                  <Minus size={24} />
-                </Button>
-                <FormControl
-                  type="text"
-                  className="dv_input_qty"
-                  defaultValue={2}
-                />
-                <Button type="button" className="btn-default dv_plus_minusbtn">
-                  <Plus size={24} />
-                </Button>
-              </ButtonGroup>
-            </div>
-          </div>
-        </li>
-        <li>
-          <div className="dv_service_prices_add_minus_btn_wrapper">
-            <div className="dv_service_prices_wrapper">
-              <span className="dv_service_name">African braids service</span>
-            </div>
-            <div className="dv_plus_minus_wrapper">
-              <ul className="dv_prices_cut_main">
-                <li>AED 120</li> <li>AED 90</li>
-              </ul>
-              <ButtonGroup size="lg">
-                <Button type="button" className="btn-default dv_plus_minusbtn">
-                  <Minus size={24} />
-                </Button>
-                <FormControl
-                  type="text"
-                  className="dv_input_qty"
-                  defaultValue={2}
-                />
-                <Button type="button" className="btn-default dv_plus_minusbtn">
-                  <Plus size={24} />
-                </Button>
-              </ButtonGroup>
-            </div>
-          </div>
-        </li>
-        <li>
-          <div className="dv_service_prices_add_minus_btn_wrapper">
-            <div className="dv_service_prices_wrapper">
-              <span className="dv_service_name">African braids service</span>
-            </div>
-            <div className="dv_plus_minus_wrapper">
-              <ul className="dv_prices_cut_main">
-                <li>AED 120</li> <li>AED 90</li>
-              </ul>
-              <ButtonGroup size="lg">
-                <Button type="button" className="btn-default dv_plus_minusbtn">
-                  <Minus size={24} />
-                </Button>
-                <FormControl
-                  type="text"
-                  className="dv_input_qty"
-                  defaultValue={2}
-                />
-                <Button type="button" className="btn-default dv_plus_minusbtn">
-                  <Plus size={24} />
-                </Button>
-              </ButtonGroup>
-            </div>
-          </div>
-        </li>
-        <li>
-          <div className="dv_service_prices_add_minus_btn_wrapper">
-            <div className="dv_service_prices_wrapper">
-              <span className="dv_service_name">African braids service</span>
-            </div>
-            <div className="dv_plus_minus_wrapper">
-              <ul className="dv_prices_cut_main">
-                <li>AED 120</li> <li>AED 90</li>
-              </ul>
-              <ButtonGroup size="lg">
-                <Button type="button" className="btn-default dv_plus_minusbtn">
-                  <Minus size={24} />
-                </Button>
-                <FormControl
-                  type="text"
-                  className="dv_input_qty"
-                  defaultValue={2}
-                />
-                <Button type="button" className="btn-default dv_plus_minusbtn">
-                  <Plus size={24} />
-                </Button>
-              </ButtonGroup>
-            </div>
-          </div>
-        </li>
-        <li>
-          <div className="dv_service_prices_add_minus_btn_wrapper">
-            <div className="dv_service_prices_wrapper">
-              <span className="dv_service_name">African braids service</span>
-            </div>
-            <div className="dv_plus_minus_wrapper">
-              <ul className="dv_prices_cut_main">
-                <li>AED 120</li> <li>AED 90</li>
-              </ul>
-              <ButtonGroup size="lg">
-                <Button type="button" className="btn-default dv_plus_minusbtn">
-                  <Minus size={24} />
-                </Button>
-                <FormControl
-                  type="text"
-                  className="dv_input_qty"
-                  defaultValue={2}
-                />
-                <Button type="button" className="btn-default dv_plus_minusbtn">
-                  <Plus size={24} />
-                </Button>
-              </ButtonGroup>
-            </div>
-          </div>
-        </li>
-        <li>
-          <div className="dv_service_prices_add_minus_btn_wrapper">
-            <div className="dv_service_prices_wrapper">
-              <span className="dv_service_name">African braids service</span>
-            </div>
-            <div className="dv_plus_minus_wrapper">
-              <ul className="dv_prices_cut_main">
-                <li>AED 120</li> <li>AED 90</li>
-              </ul>
-              <ButtonGroup size="lg">
-                <Button type="button" className="btn-default dv_plus_minusbtn">
-                  <Minus size={24} />
-                </Button>
-                <FormControl
-                  type="text"
-                  className="dv_input_qty"
-                  defaultValue={2}
-                />
-                <Button type="button" className="btn-default dv_plus_minusbtn">
-                  <Plus size={24} />
-                </Button>
-              </ButtonGroup>
-            </div>
-          </div>
-        </li>
+        {servicesJSX}
       </ul>
     </Col>
   );
