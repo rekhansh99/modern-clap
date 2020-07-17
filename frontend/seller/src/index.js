17f3fe9b0347d526375c133c07da47ee7a05bad2
Anirudh <anirudhganwal06@gmail.com>
2020-07-18 00:15:15 +0530

    imported some css and images

--- frontend/seller/src/index.js
+++ frontend/seller/src/index.js
@@ -6,6 +6,9 @@ import store from './app/store';
 import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
 
+import './index.css';
+import './responsive.css';
+
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
