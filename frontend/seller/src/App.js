bc88a0f68f90b6992e8dc183ae876f9bee5a976b
Anirudh <anirudhganwal06@gmail.com>
2020-07-18 18:24:59 +0530

    created flash cards

--- frontend/seller/src/App.js
+++ frontend/seller/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import Dashboard from './pages/Dashboard';
 import Notifications from './pages/Notifications';
@@ -10,35 +10,55 @@ import Services from './pages/Services';
 import Sale from './pages/Sale';
 import Reviews from './pages/Reviews';
 import Settings from './pages/Settings';
+import FlashCards from './components/common/FlashCards';
+import avatar from './images/avatar.png';
 
 function App() {
+  const flashCards = [
+    {
+      name: 'Rupali Sharma',
+      img: avatar,
+      service: 'Cleaners',
+      message: 'New request recieved!'
+    },
+    {
+      name: 'Rupali Sharma',
+      img: avatar,
+      service: 'Cleaners',
+      message: 'New request recieved!'
+    }
+  ];
+
   return (
     // eslint-disable-next-line no-undef
     <Router basename={process.env.PUBLIC_URL}>
-      <Route exact path="/">
-        <Dashboard />
-      </Route>
-      <Route exact path="/notifications">
-        <Notifications />
-      </Route>
-      <Route exact path="/requests">
-        <Requests />
-      </Route>
-      <Route exact path="/accepted-requests">
-        <AcceptedRequests />
-      </Route>
-      <Route exact path="/services">
-        <Services />
-      </Route>
-      <Route exact path="/sale">
-        <Sale />
-      </Route>
-      <Route exact path="/reviews">
-        <Reviews />
-      </Route>
-      <Route exact path="/settings">
-        <Settings />
-      </Route>
+      <Switch>
+        <Route exact path="/">
+          <Dashboard />
+        </Route>
+        <Route exact path="/notifications">
+          <Notifications />
+        </Route>
+        <Route exact path="/requests">
+          <Requests />
+        </Route>
+        <Route exact path="/accepted-requests">
+          <AcceptedRequests />
+        </Route>
+        <Route exact path="/services">
+          <Services />
+        </Route>
+        <Route exact path="/sale">
+          <Sale />
+        </Route>
+        <Route exact path="/reviews">
+          <Reviews />
+        </Route>
+        <Route exact path="/settings">
+          <Settings />
+        </Route>
+      </Switch>
+      <FlashCards cards={flashCards} />
     </Router>
   );
 }
