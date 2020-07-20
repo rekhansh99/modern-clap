c1eb5176d136a6816fe856fe9c30180d1e56ab50
rekhansh99 <priyadarshirekhansh@gmail.com>
2020-07-21 00:41:47 +0530

    Dashboard broken into subcomponents

---
+++ frontend/seller/src/components/Dashboard/LatestBookingsTable.js
@@ -0,0 +1,84 @@
+import React from 'react';
+
+import { Link } from 'react-router-dom';
+import { Table, Dropdown } from 'react-bootstrap';
+import { MoreVertical } from 'react-feather';
+
+import Search from '../common/Search';
+import Pagination from '../common/Pagination';
+
+const LatestBookingsTable = () => {
+  const data = [
+    {
+      requestNo: 'MD542154',
+      customer: 'sanjay sakpal',
+      service: 'waxing',
+      location: 'al rigga',
+      amount: 'AED 450',
+      status: 'pending'
+    },
+    {
+      requestNo: 'MD542154',
+      customer: 'sanjay sakpal',
+      service: 'waxing',
+      location: 'al rigga',
+      amount: 'AED 450',
+      status: 'completed'
+    }
+  ];
+
+  return (
+    <>
+      <Search />
+      <Table id="dataTable" width="100%" cellSpacing={0} responsive>
+        <thead>
+          <tr>
+            <th>request no </th>
+            <th>Customer </th>
+            <th>Service</th>
+            <th>Location</th>
+            <th>Amount</th>
+            <th>status</th>
+            <th>action</th>
+          </tr>
+          <tr>
+            <th>1542 </th>
+            <th />
+            <th />
+            <th />
+            <th>AED 12,024 </th>
+            <th />
+            <th />
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((req, index) => (
+            <tr key={index}>
+              <td>{req.requestNo}</td>
+              <td>{req.customer}</td>
+              <td>{req.service}</td>
+              <td>{req.location}</td>
+              <td>{req.amount}</td>
+              <td>
+                <span className={req.status}>{req.status}</span>
+              </td>
+              <Dropdown as="td">
+                <Dropdown.Toggle as="a" className="dv_everytable_action">
+                  <MoreVertical />
+                </Dropdown.Toggle>
+                <Dropdown.Menu alignRight>
+                  <Dropdown.Item as={Link} to="/request/MD542154">
+                    View
+                  </Dropdown.Item>
+                </Dropdown.Menu>
+              </Dropdown>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+      <Pagination />
+    </>
+  );
+};
+
+export default LatestBookingsTable;
--- frontend/seller/src/pages/Dashboard.js
+++ frontend/seller/src/pages/Dashboard.js
@@ -1,15 +1,12 @@
 import React from 'react';
 
-import { Link } from 'react-router-dom';
-import { MoreVertical } from 'react-feather';
-import { Container, Row, Col, Card, Table, Dropdown } from 'react-bootstrap';
+import { Container, Row, Col, Card } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 import NumberCard from '../components/common/NumberCard';
 import SwitchBusiness from '../components/common/SwitchBusiness';
-import Pagination from '../components/common/Pagination';
-import Search from '../components/common/Search';
 import AreaChart from '../components/Dashboard/AreaChart';
+import LatestBookingsTable from '../components/Dashboard/LatestBookingsTable';
 
 const Dashboard = () => {
   document.title = 'Dashboard - Modernclap';
@@ -29,28 +26,28 @@ const Dashboard = () => {
       <h1 className="mt-4 dv_page_heading">Dashboard</h1>
       <Row>
         <Col xs={12} md={6} xl={3}>
-          <NumberCard title="Total Appointments" value={21} />
+          <NumberCard title="Total Appointments" value={32} />
         </Col>
         <Col xs={12} md={6} xl={3}>
-          <NumberCard title="Total Appointments" value={21} />
+          <NumberCard title="Completed" value={21} />
         </Col>
         <Col xs={12} md={6} xl={3}>
-          <NumberCard title="Total Appointments" value={21} />
+          <NumberCard title="In Progress" value={7} />
         </Col>
         <Col xs={12} md={6} xl={3}>
-          <NumberCard title="Total Appointments" value={21} />
+          <NumberCard title="Rejected" value={4} />
         </Col>
         <Col xs={12} md={6} xl={3}>
-          <NumberCard title="Total Appointments" value={21} />
+          <NumberCard title="Average Rating" value={4.0} />
         </Col>
         <Col xs={12} md={6} xl={3}>
-          <NumberCard title="Total Appointments" value={21} />
+          <NumberCard title="Tools Rating" value={3.9} />
         </Col>
         <Col xs={12} md={6} xl={3}>
-          <NumberCard title="Total Appointments" value={21} />
+          <NumberCard title="Work Rating" value={4.1} />
         </Col>
         <Col xs={12} md={6} xl={3}>
-          <NumberCard title="Total Appointments" value={21} />
+          <NumberCard title="Behaviour Rating" value={4.0} />
         </Col>
       </Row>
       <Row>
@@ -65,6 +62,8 @@ const Dashboard = () => {
             </Card.Body>
           </Card>
         </Col>
+      </Row>
+      <Row>
         <Col xs={12}>
           <Card className="mb-4">
             <Card.Header>
@@ -72,76 +71,7 @@ const Dashboard = () => {
               Latest Bookings
             </Card.Header>
             <Card.Body className="p-0">
-              <Search />
-              <Table id="dataTable" width="100%" cellSpacing={0} responsive>
-                <thead>
-                  <tr>
-                    <th>request no </th>
-                    <th>Customer </th>
-                    <th>Service</th>
-                    <th>Location</th>
-                    <th>Amount</th>
-                    <th>status</th>
-                    <th>action</th>
-                  </tr>
-                  <tr>
-                    <th>1542 </th>
-                    <th />
-                    <th />
-                    <th />
-                    <th>AED 12,024 </th>
-                    <th> </th>
-                    <th />
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <td>MD542154</td>
-                    <td>sanjay sakpal </td>
-                    <td>waxing</td>
-                    <td>al rigga </td>
-                    <td>AED 450 </td>
-                    <td>
-                      <span style={{ color: 'red' }}>pending</span>
-                    </td>
-                    <Dropdown as="td">
-                      <Dropdown.Toggle as="a" className="dv_everytable_action">
-                        <MoreVertical />
-                      </Dropdown.Toggle>
-                      <Dropdown.Menu alignRight>
-                        <Dropdown.Item
-                          as={Link}
-                          className="dropdown-item"
-                          to="/request/MD542154"
-                        >
-                          View
-                        </Dropdown.Item>
-                      </Dropdown.Menu>
-                    </Dropdown>
-                  </tr>
-                  <tr>
-                    <td>MD542154</td>
-                    <td>sanjay sakpal </td>
-                    <td>waxing</td>
-                    <td>al rigga </td>
-                    <td>AED 450 </td>
-                    <td>
-                      <span style={{ color: 'green' }}>completed</span>
-                    </td>
-                    <Dropdown as="td">
-                      <Dropdown.Toggle as="a" className="dv_everytable_action">
-                        <MoreVertical />
-                      </Dropdown.Toggle>
-                      <Dropdown.Menu alignRight>
-                        <Dropdown.Item as={Link} to="/request/MD542154">
-                          View
-                        </Dropdown.Item>
-                      </Dropdown.Menu>
-                    </Dropdown>
-                  </tr>
-                </tbody>
-              </Table>
-              <Pagination />
+              <LatestBookingsTable />
             </Card.Body>
           </Card>
         </Col>
