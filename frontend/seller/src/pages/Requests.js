9945b8707b1ed40f42f601776eb4a4f23d23711c
rekhansh99 <priyadarshirekhansh@gmail.com>
2020-07-20 09:15:49 +0530

    Added Total requests sent span in requests.js

--- frontend/seller/src/pages/Requests.js
+++ frontend/seller/src/pages/Requests.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container } from 'react-bootstrap';
 
 import SwitchBusiness from '../components/common/SwitchBusiness';
 import RequestCard from '../components/Requests/RequestCard';
@@ -22,60 +22,66 @@ const Requests = () => {
         ]}
       />
       <h1 className="mt-4 dv_page_heading">Request List</h1>
-      <Row>
-        <Col xs={12}>
-          <span
-            style={{
-              color: '#adadad',
-              display: 'block',
-              margin: '0 0 10px 0',
-              textTransform: 'capitalize'
-            }}
-          >
-            pending request
-          </span>
-          <RequestCard
-            orderId="MD12345"
-            name="rupali sharma"
-            img={avatar}
-            services={['Haircut', 'Spa']}
-            payment={{ currency: 'AED', total: 250 }}
-            status="Requested Accepted"
-          />
-          <RequestCard
-            orderId="MD12345"
-            name="rupali sharma"
-            img={avatar}
-            services={['Haircut', 'Spa']}
-            payment={{ currency: 'AED', total: 250 }}
-            status="Requested Accepted"
-          />
-          <RequestCard
-            orderId="MD12345"
-            name="rupali sharma"
-            img={avatar}
-            services={['Haircut', 'Spa']}
-            payment={{ currency: 'AED', total: 250 }}
-            status="Requested Accepted"
-          />
-          <RequestCard
-            orderId="MD12345"
-            name="rupali sharma"
-            img={avatar}
-            services={['Haircut', 'Spa']}
-            payment={{ currency: 'AED', total: 250 }}
-            status="Requested Accepted"
-          />
-          <RequestCard
-            orderId="MD12345"
-            name="rupali sharma"
-            img={avatar}
-            services={['Haircut', 'Spa']}
-            payment={{ currency: 'AED', total: 250 }}
-            status="Requested Accepted"
-          />
-        </Col>
-      </Row>
+      <span
+        style={{
+          color: '#adadad',
+          display: 'block',
+          margin: '0 0 10px 0',
+          textTransform: 'capitalize'
+        }}
+      >
+        pending request
+      </span>
+      <RequestCard
+        orderId="MD12345"
+        name="rupali sharma"
+        img={avatar}
+        services={['Haircut', 'Spa']}
+        payment={{ currency: 'AED', total: 250 }}
+        status="Requested Accepted"
+      />
+      <RequestCard
+        orderId="MD12345"
+        name="rupali sharma"
+        img={avatar}
+        services={['Haircut', 'Spa']}
+        payment={{ currency: 'AED', total: 250 }}
+        status="Requested Accepted"
+      />
+      <RequestCard
+        orderId="MD12345"
+        name="rupali sharma"
+        img={avatar}
+        services={['Haircut', 'Spa']}
+        payment={{ currency: 'AED', total: 250 }}
+        status="Requested Accepted"
+      />
+      <RequestCard
+        orderId="MD12345"
+        name="rupali sharma"
+        img={avatar}
+        services={['Haircut', 'Spa']}
+        payment={{ currency: 'AED', total: 250 }}
+        status="Requested Accepted"
+      />
+      <RequestCard
+        orderId="MD12345"
+        name="rupali sharma"
+        img={avatar}
+        services={['Haircut', 'Spa']}
+        payment={{ currency: 'AED', total: 250 }}
+        status="Requested Accepted"
+      />
+      <span
+        style={{
+          color: '#adadad',
+          display: 'block',
+          margin: '0 0 10px 0',
+          textTransform: 'inherit'
+        }}
+      >
+        Total we have sent you <strong>120 request </strong>
+      </span>
     </Container>
   );
 };
