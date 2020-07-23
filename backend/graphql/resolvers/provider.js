b1dd6e85104bc05135f61b075627f5b30db71b13
Anirudh <anirudhganwal06@gmail.com>
2020-07-23 19:08:25 +0530

    provider login resolver done

--- backend/graphql/resolvers/provider.js
+++ backend/graphql/resolvers/provider.js
@@ -1,5 +1,7 @@
-const Provider = require("../../models/provider");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
+const Provider = require("../../models/provider");
 const { transformProvider } = require("./transformers/provider");
 
 module.exports = {
@@ -11,19 +13,46 @@ module.exports = {
     // TODO
   },
 
-  registerProvider: async (args, req) => {
+  registerProvider: async (args) => {
+    const hashedPassword = await bcrypt.hash(args.provider.password, 12);
     const newProvider = new Provider({
       ...args.provider,
+      password: hashedPassword,
     });
     const provider = await newProvider.save();
     return transformProvider(provider);
   },
 
-  loginProvider: (args, req) => {
-    // TODO
+  loginProvider: async (args, ctx) => {
+    const provider = await Provider.findOne({ ownerEmail: args.email });
+    if (!provider) {
+      throw new Error("User not found!");
+    }
+    const passwordMatch = await bcrypt.compare(
+      args.password,
+      provider.password
+    );
+    if (!passwordMatch) {
+      throw new Error("Password not matched!");
+    }
+    const jwtValidity = 60 * 60 * 24 * 30; // in seconds
+    const token = jwt.sign(
+      {
+        uid: provider.id,
+        role: "provider",
+        exp: Math.floor(Date.now() / 1000) + jwtValidity,
+      },
+      process.env.JWT_SECRET
+    );
+    ctx.res.cookie("jwt", token, {
+      httpOnly: true,
+      secure: true,
+      maxAge: jwtValidity * 1000,
+    });
+    return transformProvider(provider);
   },
 
   updateProvider: (args, req) => {
     // TODO
-  }
+  },
 };
