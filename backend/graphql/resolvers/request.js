e7912407087bc35dd48031c5c7a44ed479ab6247
Anirudh <anirudhganwal06@gmail.com>
2020-07-24 14:15:19 +0530

    created some request resolvers

--- backend/graphql/resolvers/request.js
+++ backend/graphql/resolvers/request.js
@@ -1,4 +1,5 @@
 const Request = require('../../models/request');
+const Customer = require('../../models/customer');
 const { transformRequest, requests } = require('./transformers/request');
 
 module.exports = {
@@ -16,12 +17,53 @@ module.exports = {
     return requests(args.id);
   },
 
-  bookRequest: () => {
-    // TODO
+  bookRequest: async (args, ctx) => {
+    const newRequest = new Request({
+      serivces: args.request.serivces.map(service => ({
+        service: service,
+        staffAssigned: []
+      })),
+      time: args.request.time,
+      location: {
+        flatNo: args.request.flatNo,
+        area: args.request.area,
+        landmark: args.request.landmark,
+        addressType: args.request.addressType
+      },
+      payment: {
+        mode: args.request.mode,
+        subTotal: args.request.subTotal,
+        vat: args.request.vat,
+        discount: args.request.discount,
+        total: args.request.total
+      },
+      status: 'pending',
+      customer: ctx.req.uid
+    });
+    const request = await newRequest.save();
+
+    await Customer.findByIdAndUpdate(ctx.req.uid, {
+      $push: {
+        pendingRequests: request.id
+      }
+    });
+
+    return transformRequest(request);
   },
 
-  cancelRequest: () => {
-    // TODO
+  cancelRequest: async (args, ctx) => {
+    await Request.findByIdAndDelete(args.id);
+
+    await Customer.findByIdAndUpdate(ctx.req.uid, {
+      $pull: {
+        pendingRequests: args.id
+      },
+      $push: {
+        cancelledRequests: args.id
+      }
+    });
+
+    return 'Request deleted successfully!';
   },
 
   updateRequest: () => {
