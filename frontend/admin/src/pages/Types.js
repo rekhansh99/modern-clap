b1e63de6a03d5d792dcb31b2445f35791206a90d
Anirudh <anirudhganwal06@gmail.com>
2020-07-26 12:58:39 +0530

    created types page in admin panel

--- frontend/admin/src/pages/Types.js
+++ frontend/admin/src/pages/Types.js
@@ -1,7 +1,29 @@
 import React from 'react';
+import { Container, Card } from 'react-bootstrap';
+import Search from '../components/common/Search';
+import { Link } from 'react-router-dom';
+import TypesTable from '../components/Types/TypesTable';
+import Pagination from '../components/common/Pagination';
 
-const Dashboard = () => {
-  return <h1>I am a page</h1>;
+const Types = () => {
+  return (
+    <Container fluid>
+      <h1 className="mt-4 dv_page_heading">Types</h1>
+      <Card className="mb-4">
+        <Card.Header>
+          All Types
+          <Link to="/services/add" className="add_new_button">
+            add new
+          </Link>
+        </Card.Header>
+        <Card.Body className="p-0">
+          <Search />
+          <TypesTable types={[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]} />
+          <Pagination />
+        </Card.Body>
+      </Card>
+    </Container>
+  );
 };
 
-export default Dashboard;
+export default Types;
