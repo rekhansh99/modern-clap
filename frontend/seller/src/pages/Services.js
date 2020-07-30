db768d06744d814a88bd24464e6f0b27112f9691
Anirudh <anirudhganwal06@gmail.com>
2020-07-30 17:35:01 +0530

    services are being fetched from serviceTable instead of services

--- frontend/seller/src/pages/Services.js
+++ frontend/seller/src/pages/Services.js
@@ -1,60 +1,12 @@
 import React from 'react';
-import { gql, useQuery } from '@apollo/client';
 import { Link } from 'react-router-dom';
 import { Container, Card } from 'react-bootstrap';
 
 import SwitchBusiness from '../components/common/SwitchBusiness';
 import ServicesTable from '../components/Services/ServicesTable';
 
-const GET_SERVICES = gql`
-  query Services($id: ID!) {
-    provider(id: $id) {
-      businessCategories {
-        name
-        type {
-          name
-        }
-        services {
-          _id
-          name
-        }
-      }
-      services {
-        serviceId
-        active
-        salePrice
-      }
-    }
-  }
-`;
-
 const Services = () => {
   document.title = 'Services - Modernclap';
-
-  const { loading, error, data: getServicesData } = useQuery(GET_SERVICES, {
-    variables: {
-      id: '5f198e857ae7adef1dc89c86'
-    }
-  });
-
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>An error occured!</p>;
-
-  let services = [];
-  for (let category of getServicesData.provider.businessCategories) {
-    for (let service of category.services) {
-      services.push({
-        id: service._id,
-        name: service.name,
-        category: category.name,
-        type: category.type.name
-      });
-    }
-  }
-  services = services.map(service => ({
-    ...service,
-    ...getServicesData.provider.services.find(s => s.serviceiId === service._id)
-  }));
 
   return (
     <Container fluid>
@@ -77,7 +29,7 @@ const Services = () => {
           </Link>
         </Card.Header>
         <Card.Body className="p-0">
-          <ServicesTable services={services} />
+          <ServicesTable />
         </Card.Body>
       </Card>
     </Container>
